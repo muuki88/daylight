43ea121c5b309e493882ef94fc61363d9cf39d0b
Nepomuk Seiler <nepomuk.seiler@gutefrage.net>
2017-01-09 10:51:52 +0100

    Merged api-ai models

--- src/api-ai/Interfaces.ts
+++ src/api-ai/Interfaces.ts
@@ -8,21 +8,31 @@ export interface IRequestOptions {
 }
 export interface IServerResponse {
     id?: string;
-    result?: {
-        action: string,
-        resolvedQuery: string,
-        speech: string;
-        fulfilment?: {
-            speech: string
-        }
-    };
+    result?: IAction,
     status: {
         code: number,
         errorDetails?: string,
         errorID?: string,
         errorType: string
     };
+};
+
+export interface IAction {
+  action: string,
+  resolvedQuery: string,
+  actionIncomplete: boolean,
+  contexts: Array<IContext>,
+  fulfillment?: {
+      speech: string
+  }
 }
+
+export interface IContext {
+  name: string,
+  parameters?: {},
+  lifespan: number
+}
+
 export interface IStringMap { [s: string]: string; }
 export interface IApiClientOptions {
     lang?: Constants.AVAILABLE_LANGUAGES;
